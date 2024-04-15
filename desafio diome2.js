15e61ae0458578cbd8348248ffeed1ff845331ec
anderson rocha oficial <157828876+Andersonarcher@users.noreply.github.com>
2024-04-15 08:48:04 -0300

    Update desafio diome2.js

--- desafio diome2.js
+++ desafio diome2.js
@@ -7,15 +7,19 @@ function calcular(vitorias,derrotas){
                                     
                                  
            //classificaçao por nivel do heroi e mensagem:o heroi tem de saldo:*(calculo/vitorias)* e esta no nivel *(nivel)*
-                  if(calculo<=10){nivel="ferro" ;console.log("o heroi tem de saldo "+ calculo+" está no nivel "+nivel)}
-         else if(calculo>=11&&calculo<=20){nivel="bronze";console.log("o heroi tem de saldo "+ calculo+" e está no nivel "+nivel)}
-         else if(calculo>=21&&calculo<=50){nivel="prata";console.log("o heroi tem de saldo "+ calculo+" e está no nivel "+nivel)}                        
-        else if(calculo>=51&&calculo<=80){nivel="ouro";console.log("o heroi tem de saldo "+ calculo+"  e está no nivel "+nivel )}
-         else if(calculo>=81&&calculo<=90){nivel="diamante";console.log("o heroi tem de saldo "+ calculo+" e está no nivel "+nivel)}
-         else if(calculo>=91&&calculo<=100){nivel="lendario";console.log("o heroi tem de saldo "+ calculo+ " e está no nivel "+nivel)}
-          else {nivel="imortal";console.log("o heroi tem de saldo "+ calculo+" e está no nivel "+nivel)} }
+                  //classificaçao por nivel do heroi e mensagem:o heroi tem de saldo:*(calculo/vitorias)* e esta no nivel *(nivel)*
+                  if(calculo<=10){nivel="ferro" }
+         else if(calculo>=11&&calculo<=20){nivel="bronze"}
+         else if(calculo>=21&&calculo<=50){nivel="prata"}                        
+        else if(calculo>=51&&calculo<=80){nivel="ouro"}
+         else if(calculo>=81&&calculo<=90){nivel="diamante"}
+         else if(calculo>=91&&calculo<=100){nivel="lendario"}
+          else {nivel="imortal"
+               }
+//mensagem oficial
+  console.log("o heroi tem de saldo "+ calculo+" e está no nivel "+nivel)}
+   calcular(100,16)
                 
-      
            
                                   
  
